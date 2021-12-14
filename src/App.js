975499f85d639d31090ff283d06e5808fc48a0d8
Parkstelth <stelthicmaster@gmail.com>
2021-12-14 15:39:23 +0900

    change 2 / add Inferface

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+import Kryptokitty from "./components/Kryptokitty";
+
+function App() {
+  return (
+    <div className="App">
+      <Kryptokitty />
+    </div>
+  );
+}
+
+export default App;
