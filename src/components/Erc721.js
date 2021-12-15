a2ca63ee8f4f302519460bd849537a33b3d9f193
Parkstelth <stelthicmaster@gmail.com>
2021-12-15 23:38:08 +0900

    change 8 / add call NFT system

--- src/components/Erc721.js
+++ src/components/Erc721.js
@@ -1,37 +1,44 @@
 import axios from 'axios'
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 function Erc721({ erc721list }) {
 
+    const [list, setList] = useState([]);
 
-async function findURI(uri){
-  
-       const res = await axios.get(uri)
-           console.log('test1',res.data.image);
- 
-}
+    async function findURI(uri){
+        const res = await axios.get(uri)
+        console.log('test1',res.data.image);
+        return res.data.image;
+    }
 
-let erc721item =[]
-for(let item of erc721list){
-    erc721item.push(item)
-}
-console.log(erc721item)
+    useEffect(() => {
+        (async () => {
+            const arrElem = [];
+            for (const token of erc721list) {
+                console.log(token);
+                const src = await findURI(token.tokenURI);
+                console.log(src)
+                arrElem.push(
+                <div className="erc721token">
+                Name: <span className="name">{token.name}</span>(
+                <span className="symbol">{token.symbol}</span>)
+
+                <div className="nft">id: {token.tokenId}</div>
+                {/* {findURI(token.tokenURI)} */}
+                <img src={src} width={300} />
+            </div>
+            );
+            }
+            setList(arrElem)
+        })();
+    }, [])
+
+    console.log('arr', list)
+
     return (
         <div className="erc721list">
-            {erc721item.map(async(token) => {
-                return (
-                    <div className="erc721token">
-                        Name: <span className="name">{token.name}</span>(
-                        <span className="symbol">{token.symbol}</span>)
-                        
-                        <div className="nft">id: {token.tokenId}</div>                   
-                        <img src={await findURI(token.tokenURI)} width={300} />
-                   
-                    </div>
-                );
-            })}
-          
+            {list.map((elem) => elem)}
         </div>
     );
 }
-
+//0x29a55a08460de2eF991d8B5e14F83A681C424520
 export default Erc721;
