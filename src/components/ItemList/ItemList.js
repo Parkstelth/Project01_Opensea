496a274e33d8afb49797b6181f2da9890a00af48
Parkstelth <stelthicmaster@gmail.com>
2021-12-16 15:54:40 +0900

    Browse update

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Items from './Items';
+import './ItemList.css'
+
+
 const ItemList = ({ data }) => {
+  console.log(data)
   return (
-    <div>
+    <div className="card-group addOption">
       {data.map((item) => (
-        <img src={item.image_url} />
+        <div className="card addOption">
+        <img src={item.image_url} className="card-img-top" alt="..." />
+        <div className="card-body addOption">
+          <h5 className="card-title addOption">{item.name}</h5>
+          {/* <p className="card-text">{item.description}</p> */}
+          <p className="card-text addOption"><button type="button" class="btn btn-secondary">상세보기</button></p>
+        </div>
+      </div>
+     
       ))}
     </div>
   );
 };
 
 export default ItemList;
+
