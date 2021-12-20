70a667ec6041169df3d7aaf4925a1f189ea3ab1a
sonny6786 <3076270s@gmail.com>
2021-12-20 14:01:55 +0900

    Changed main web component with css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
 // import logo from "./logo.svg";
-import './App.css';
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Create from './components/Create';
-import Home from './components/Home';
-import { useState, useEffect } from 'react';
-import Nav from './components/Nav';
-import Activity from './components/Activity';
-import Browse from './components/Browse';
-import About from './components/About';
+import Create from "./components/Create";
+import Home from "./components/Home";
+import { useState, useEffect } from "react";
+import Nav from "./components/Nav";
+import Activity from "./components/Activity";
+import Browse from "./components/Browse";
+import About from "./components/About";
 function App() {
-  const [mainaccount, setMainaccount] = useState('Address not yet!');
-  const [mainweb3, setMainweb3] = useState('not load web3');
-  const [clickedItem, setClickedItem] = useState();
-  useEffect(() => {
-    setMainaccount(mainaccount);
-  }, [mainaccount]);
+    const [mainaccount, setMainaccount] = useState("Address not yet!");
+    const [mainweb3, setMainweb3] = useState("not load web3");
+    const [clickedItem, setClickedItem] = useState();
+    const [isLogin, setIsLogin] = useState(false);
 
-  const onClickItem = (e) => {
-    setClickedItem(e);
-  };
+    useEffect(() => {
+        setMainaccount(mainaccount);
+    }, [mainaccount]);
 
-  return (
-    <BrowserRouter>
-      <Nav setmainaccount={setMainaccount} setmainweb3={setMainweb3} />
-      <Routes>
-        <Route exact={true} path="/" element={<Home account={mainaccount} />} />
-        <Route path="/browse" element={<Browse onClickItem={onClickItem} />} />
-        <Route path="/create" element={<Create account={mainaccount} />} />
-        <Route
-          path="/activity"
-          element={<Activity account={mainaccount} web3={mainweb3} />}
-        />
-        <Route
-          path={`/browse/${clickedItem}`}
-          element={<About clickedItem={clickedItem} />}
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+    const onClickItem = (e) => {
+        setClickedItem(e);
+    };
+
+    useEffect(() => {
+        if (mainaccount === "Address not yet!") {
+            setIsLogin(false);
+        } else {
+            setIsLogin(true);
+        }
+    }, [mainaccount]);
+
+    return (
+        <BrowserRouter>
+            <Nav setmainaccount={setMainaccount} setmainweb3={setMainweb3} login={isLogin} />
+            <Routes>
+                <Route exact={true} path="/" element={<Home account={mainaccount} />} />
+                <Route path="/browse" element={<Browse onClickItem={onClickItem} />} />
+                <Route path="/create" element={<Create account={mainaccount} />} />
+                <Route path="/activity" element={<Activity account={mainaccount} web3={mainweb3} />} />
+                <Route path={`/browse/${clickedItem}`} element={<About clickedItem={clickedItem} />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,37 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import ItemList from './ItemList/ItemList';
-import Items from './ItemList/Items';
-import './Browse.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList/ItemList";
+import Items from "./ItemList/Items";
+import "./Browse.css";
 const Browse = ({ onClickItem }) => {
-  const [data, setData] = useState([]);
-  const [clickedData, setClickedData] = useState([]);
-  useEffect(() => {
-    const options = { method: 'GET' };
+    const [data, setData] = useState([]);
+    const [clickedData, setClickedData] = useState([]);
+    useEffect(() => {
+        const options = { method: "GET" };
 
-    const dataLoad = async () => {
-      const dataList = await fetch(
-        'https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20',
-        options
-      )
-        .then((response) => response.json())
-        .catch((err) => console.error(err));
-      setData(dataList.assets);
-      console.log(dataList.assets);
+        const dataLoad = async () => {
+            const dataList = await fetch("https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20", options)
+                .then((response) => response.json())
+                .catch((err) => console.error(err));
+            setData(dataList.assets);
+            console.log(dataList.assets);
+        };
+        dataLoad();
+    }, []);
+
+    const onClickedItem = (e) => {
+        console.log(e);
+        onClickItem(e);
     };
-    dataLoad();
-  }, []);
 
-  const onClickedItem = (e) => {
-    console.log(e);
-    onClickItem(e);
-  };
-
-  return (
-    <div className="browse-container">
-      <ItemList data={data} onClickedItem={onClickedItem}></ItemList>
-    </div>
-  );
+    return (
+        <div className="browse-container">
+            <ItemList data={data} onClickedItem={onClickedItem}></ItemList>
+        </div>
+    );
 };
 
 export default Browse;
